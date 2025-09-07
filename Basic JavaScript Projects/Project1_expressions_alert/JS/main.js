51a521569d1e048c8657988cb3f9995f020a9ea6
micheallaird <micheallaird@yahoo.com>
2025-09-06 19:06:39 -0600

    Added Project2 folder

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -10,10 +10,6 @@ document.write(A);
 // Using escape characters to include quotes in a string
 document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell Dad!\"<br>\"Eat my shorts!\" Bart responded.");
 document.write(`\"Be who you are and say what you feel, because those who mind don\'t matter, and those who matter don\'t mind.\"-Dr. Seuss`);
-// Concatenating strings
-var Sent1 = "This is the beginning of the string";
-var Sent2 = "and this is the end of the string.";
-document.write(Sent1 + " " + Sent2);
 // More variable definitions and simple arithmetic
 var Family = "The Lairds", Dad = "Micheal", Mom = "Sonya", Daughter = "Virginia", Son = "Austin";
 document.write(Dad);
---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+// Concatenating strings
+var Sent1 = "This is the beginning of the string";
+var Sent2 = "and this is the end of the string.";
+document.write(Sent1 + " " + Sent2);
+// Example of getElementById
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
