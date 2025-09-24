66535241010d61efe0671428253700c6e77eb47a
micheallaird <micheallaird@yahoo.com>
2025-09-23 20:07:11 -0600

    Ajax Project

---
+++ Advanced JavaScript Projects/AJAX/ajax_challenge.js
@@ -0,0 +1,12 @@
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+     document.getElementById("response").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "ajax_response.html", true);
+  xhttp.send();
+}
+
+
